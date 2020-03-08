ffe156f0ff5dbf452823e89905f81be64897dbae
Bekir Inalhan <binalhan_63@hotmail.com>
2020-03-07 23:56:42 -0600

    added loop practice file

---
+++ src/Day1/LoopPractice.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+        //somme random code
+        for ( int i=0; i<10; i+=2) {
+            System.out.println("counter : :"+i);
+
+        }
+    }
+
+
+
+}
