5556ef78f25b1b20f9a65d2092b44a38d6ace1cf
Bekir Inalhan <binalhan_63@hotmail.com>
2020-03-07 23:36:47 -0600

    modified stringPractice class

--- src/Day1/stringPractice.java
+++ src/Day1/stringPractice.java
@@ -3,5 +3,8 @@ package Day1;
 public class stringPractice {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+    String str = "java is fum";
+        System.out.println(str);
     }
 }
